bde5aa85cb3810eebf5cfb2d14208d137de3ec71
Ngas Wakefield <ngaswakefield@gmail.com>
2024-12-08 16:37:11 +1300

    add components and placeholders

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { PaletteComponent, ImgSrcComponent, DropdownComponent } from './components/index'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PaletteComponent, ImgSrcComponent, DropdownComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'nzpalette';
+  title = 'te taiao';
+  subtitleMi = 'Tae o te Whenua - Colours of the Earth ';
+  subtitleEng = 'The colours of nature'
+  logo = 'assets/nz-palette-logo.png';
 }
